b99224d4c13329dd3d9330ab41b26f13c2dbfcc0
parus1412 <parus1@i.ua>
2017-07-11 01:02:59 +0300

    fix connection

--- src/EchoServer/EchoServer.java
+++ src/EchoServer/EchoServer.java
@@ -1,8 +1,12 @@
 package EchoServer;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import MyEchoException.MyEchoException;
@@ -11,23 +15,24 @@ import static com.sun.jndi.ldap.LdapCtx.DEFAULT_PORT;
 
 public class EchoServer {
     private int port;
-    private boolean isRuning;
+    private boolean isRunning;
     private ServerSocket serverSocket;
+//    private ArrayList connections;
 
     public EchoServer() throws MyEchoException, IOException {
         this(DEFAULT_PORT);
-        this.isRuning = false;
-        //this.serverSocket = new ServerSocket(this.port);
+        this.isRunning = false;
+//        this.connections = new ArrayList();
     }
 
     public EchoServer(int port) throws IOException {
         this.port = port;
-        this.isRuning = false;
-        //this.socket = new ServerSocket(this.port);
+        this.isRunning = false;
+//        this.connections = new ArrayList();
     }
 
     public boolean isRunning() {
-        return isRuning;
+        return isRunning;
     }
 
     public int getPort() {
@@ -35,36 +40,46 @@ public class EchoServer {
     }
 
     public void start() throws IOException {
-        if ( !isRuning ) {
-            this.isRuning = !this.isRuning;
-            System.out.println("Start Echo Server");
-
+        if ( !isRunning() ) {
+            this.isRunning = !this.isRunning;
             this.serverSocket = new ServerSocket(this.port);
-
+            System.out.println("Echo Server started");
         }
 
 
-        while(isRuning) {
+        while( isRunning() ) {
             Socket clientSocket = serverSocket.accept();
             System.out.println("Accepted connection from client");
 
-            while ( isRuning ) {
-                String s;
-                Scanner in = new Scanner(clientSocket.getInputStream());
-                    s = in.next();
+                BufferedReader out = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                String clientInput;
+                System.out.println(clientSocket.isConnected());
+
+                while ((clientInput = out.readLine()) != null) {
+                    System.out.println("Client input: " + clientInput);
+                }
 
 
-                System.out.println(s);
-            }
+
+
+//                BufferedReader in = new InputStreamReader(clientSocket.getInputStream());
+//                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+//                BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+//                String userInput;
+//
+//                while ((userInput = stdIn.readLine()) != null) {
+//                    out.println(userInput);
+//                    System.out.println("echo: " + in.readLine());
+//                }
             System.out.println("Disconnected from client");
 
         }
     }
 
     public void stop() throws IOException {
-        if ( isRuning ) {
-            //serverSocket.close();
-            this.isRuning = !this.isRuning;
+        if ( isRunning ) {
+            serverSocket.close();
+            this.isRunning = !this.isRunning;
         }
         System.out.println("Stop Echo Server");
     }
